645d68b24e23f899f6dcc29939f9ec47941aba95
Cazaciuc Gabriel <gcazaciuc@yahoo.com>
2021-09-11 13:49:15 +0000

    Add tests for the common cases

--- src/__tests__/tfrpspec.js
+++ src/__tests__/tfrpspec.js
@@ -1,21 +1,110 @@
+import { jest } from '@jest/globals';
 import { observable, computed, effect } from '../tfrp';
 
 describe("TinyState", () => {
-    test("Should allow creating observables out of data", () => {
+    test("Should allow creating observables out of object data", () => {
         const o = observable({firstName: 'a', lastName: 'b'});
         expect(o.firstName).toBe('a');
         expect(o.lastName).toBe('b');
     })
 
+    test("Should allow creating observables out of primitive data", () => {
+        const o = observable(1);
+        expect(o.value).toBe(1);
+        o.value = 2;
+        expect(o.value).toBe(2);
+    })
+
     test("Should allow creating computeds", () => {
         const o = observable({firstName: 'a', lastName: 'b'});
         const c = computed(() => `${o.firstName}-${o.lastName}`);
-        expect(c).toBe('a-b');
+        expect(c.value).toBe('a-b');
+    })
+
+    test("Computed should re-run when any observed property changes", () => {
+        const o = observable({firstName: 'a', lastName: 'b'});
+        const computedFn = jest.fn(() => {
+            return `${o.firstName}-${o.lastName}`;
+        })
+        const r = computed(computedFn);
+        expect(r.value).toBe('a-b');
+        o.lastName = 'c';
+        expect(r.value).toBe('a-c');
+    })
+
+    test("Computed should not re-run if the dependencies have not changed", () => {
+        const o = observable({firstName: 'a', lastName: 'b', email: 'test@test.com'});
+        const computedFn = jest.fn(() => {
+            return `${o.firstName}-${o.lastName}`;
+        })
+        const r = computed(computedFn);
+        expect(computedFn).toHaveBeenCalledTimes(1);
+        expect(r.value).toBe('a-b');
+        o.email = 'test2@test.com';
+        expect(computedFn).toHaveBeenCalledTimes(1);
+        expect(r.value).toBe('a-b');
+    })
+
+    test("Computed should cache it's value until the dependencies have changed", () => {
+        const o = observable({firstName: 'a', lastName: 'b', email: 'test@test.com'});
+        const computedFn = jest.fn(() => {
+            return `${o.firstName}-${o.lastName}`;
+        })
+        const r = computed(computedFn);
+        expect(computedFn).toHaveBeenCalledTimes(1);
+        expect(r.value).toBe('a-b');
+        expect(r.value).toBe('a-b');
+        o.lastName = 'c';
+        expect(computedFn).toHaveBeenCalledTimes(2);
+        expect(r.value).toBe('a-c');
+    })
+
+    test("Computed should be observable by themselves", () => {
+        const o = observable({firstName: 'a', lastName: 'b', email: 'test@test.com'});
+        const computedFn = jest.fn(() => {
+            return `${o.firstName}-${o.lastName}`;
+        })
+        const r = computed(computedFn);
+        const nestedComputedFn = jest.fn(() => {
+            return r.value + 'd';
+        })
+        const n = computed(nestedComputedFn);
+        expect(computedFn).toHaveBeenCalledTimes(1);
+        expect(r.value).toBe('a-b');
+        expect(n.value).toBe('a-bd');
+        o.lastName = 'c';
+        expect(computedFn).toHaveBeenCalledTimes(2);
+        expect(nestedComputedFn).toHaveBeenCalledTimes(2);
+        expect(r.value).toBe('a-c');
+        expect(n.value).toBe('a-cd');
     })
 
     test("Should allow creating effects", () => {
         const o = observable({firstName: 'a', lastName: 'b'});
-        const c = effect(() => `${o.firstName}-${o.lastName}`);
-        expect(c).toBe('a-b');
+        const effectFn = jest.fn(() => `${o.firstName}-${o.lastName}`);
+        expect(effectFn).not.toBeCalled();
+        effect(effectFn);
+        expect(effectFn).toBeCalled();
+    })
+
+    test("Effects should re-run when any observed property changes", () => {
+        const o = observable({firstName: 'a', lastName: 'b'});
+        const effectFn = jest.fn(() => `${o.firstName}-${o.lastName}`);
+        expect(effectFn).not.toBeCalled();
+        effect(effectFn);
+        expect(effectFn).toBeCalled();
+        o.lastName = 'c';
+        expect(effectFn).toHaveBeenCalledTimes(2);
+    })
+
+    test("Effects should allow un-subscribing when any observed property changes", () => {
+        const o = observable({firstName: 'a', lastName: 'b'});
+        const effectFn = jest.fn(() => `${o.firstName}-${o.lastName}`);
+        expect(effectFn).not.toBeCalled();
+        const unsubscribe = effect(effectFn);
+        expect(effectFn).toHaveBeenCalledTimes(1);
+        unsubscribe();
+        o.lastName = 'c';
+        expect(effectFn).toHaveBeenCalledTimes(1);
     })
 })
--- src/tfrp.js
+++ src/tfrp.js
@@ -1,11 +1,61 @@
-export function observable(data) {
-    return data;
-}
 
-export function computed(fn) {
-    return fn();
+// Keep track of the derivation stack
+const stack = [];
+
+export function observable(data) {
+    let dependencies = [];
+    const targetData = typeof data === 'object' ? data : { value: data };
+    const handler = {
+        get: function(target, key, receiver) {
+            // Record subscriptions
+            const runningEffect = stack[stack.length - 1];
+            if (runningEffect) {
+                dependencies.push({runningEffect, key})
+                runningEffect.cleanup.push(() => {
+                    dependencies = dependencies.filter((d) => d.key === key && d.runningEffect.fn === runningEffect.fn);
+                });
+            }
+            return Reflect.get(target, key, receiver);
+        },
+        set: function(target, key, value, receiver) {
+            // Notify subscribers
+            const result = Reflect.set(target, key, value, receiver)
+            dependencies.filter((d) => d.key === key).forEach((d) => d.runningEffect.run());
+            return result;
+        }
+    }
+
+    return new Proxy(targetData, handler);
 }
 
 export function effect(fn) {
-    return fn();
+    const run = () => {
+        const r = fn();
+        fn.__cache__ = fn.__cache__ ? fn.__cache__ : observable(r);
+        fn.__cache__.value = r;
+    }
+    const cleanup = () => {
+        // Unsubscribe from all tracked observables
+        if (fn.__cleanup__) {
+            fn.__cleanup__.forEach((c) => c());
+        }
+    };
+    
+    cleanup();
+    const stackEntry = { run, cleanup: [] };
+    stack.push(stackEntry);
+    run();
+    fn.__cleanup__ = stackEntry.cleanup;
+    stack.pop();
+
+    return cleanup;
+}
+
+export function computed(fn) {
+    if (fn.__cache__) {
+        return fn.__cache__;
+    }
+    effect(fn);
+
+    return fn.__cache__;
 }
